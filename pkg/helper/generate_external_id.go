9d59e4f7867687dbf2acbc948c87730baba47995
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-14 21:48:56 +0500

    updated

--- pkg/helper/generate_external_id.go
+++ pkg/helper/generate_external_id.go
@@ -6,24 +6,19 @@ import (
 	"strings"
 )
 
-func GenerateExternalID(extID string) string { // (I-0099) I-0100
-	// logic
-	parts := strings.Split(extID, "-")
-	var newExtID string
-
-	if len(parts) == 2 {
-		prefix := parts[0]    // I
-		numberStr := parts[1] // 0001
-
-		number, err := strconv.Atoi(numberStr)
-		if err != nil {
-			fmt.Println("error is while converting integer part", err.Error())
-			return ""
-		}
-		number++
-
-		newExtID = fmt.Sprintf("%s-%04d", prefix, number)
+func GenerateExternalID(input string) string {
+	parts := strings.Split(input, "-")
+	if len(parts) != 2 {
+		return ""
 	}
 
-	return newExtID
+	numStr := parts[1]
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return ""
+	}
+
+	nextNum := num + 1
+	nextID := fmt.Sprintf("I-%04d", nextNum)
+	return nextID
 }
